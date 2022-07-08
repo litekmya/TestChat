c8e0f87517c874f35be8463d67d391d7e84e1f5f
Vladimir <tekmya@mail.ru>
2022-07-08 15:17:12 +0300

    Initial Commit

---
+++ TestChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestChat
+//
+//  Created by Владимир Ли on 08.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
